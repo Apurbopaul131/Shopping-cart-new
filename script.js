4d8b3403c9ad3818925ba5c8c0d87a011ab0608e
Apurbopaul131 <apurbopaul131@gmail.com>
2021-07-08 01:06:47 +0600

    reduce extra code using function

--- script.js
+++ script.js
@@ -1,33 +1,43 @@
 let plusIcon = document.querySelector('#icon-plus');
 plusIcon.addEventListener('click',(e)=>{
     e.preventDefault();
-   let phoneInput = document.querySelector('#phone-input').value;
-   let phoneInputNumber = parseInt(phoneInput);
-   let newPhoneInputNumber = phoneInputNumber + 1;
-   document.querySelector('#phone-input').value = newPhoneInputNumber;
-//    let phoneAmount = document.querySelector('#phone-amount').innerText;
-//    let phoneAmountNumber = parseInt(phoneAmount);
-   let totalPhoneAmount = 1219 * newPhoneInputNumber;
-   document.querySelector('#phone-amount').innerText = totalPhoneAmount;
+//    let phoneInput = document.querySelector('#phone-input').value;
+//    let phoneInputNumber = parseInt(phoneInput);
+//    let newPhoneInputNumber = phoneInputNumber + 1;
+//    document.querySelector('#phone-input').value = newPhoneInputNumber;
+// //    let phoneAmount = document.querySelector('#phone-amount').innerText;
+// //    let phoneAmountNumber = parseInt(phoneAmount);
+//    let totalPhoneAmount = 1219 * newPhoneInputNumber;
+//    document.querySelector('#phone-amount').innerText = totalPhoneAmount;
+
+
+
+// update code and reduce extra code using function
+productChange('#phone',true);
+let phoneInput = document.querySelector('#phone-input').value;
+let caseInput = document.querySelector('#case-input').value;
+updateSubtotal(phoneInput,caseInput);
+
+
 
 
    //subtotal features add
 
    
-   let caseInput = document.querySelector('#case-input').value;
-   let caseInputNumber = parseInt(caseInput);
-   let newSubtotal = updateSubtotal(newPhoneInputNumber,caseInputNumber);
-   document.querySelector('#subtotal').innerText =  newSubtotal;
+//    let caseInput = document.querySelector('#case-input').value;
+//    let caseInputNumber = parseInt(caseInput);
+//    let newSubtotal = updateSubtotal(newPhoneInputNumber,caseInputNumber);
+//    document.querySelector('#subtotal').innerText =  newSubtotal;
 
-   // call tax function
-   let resultTax = Tax(newSubtotal);
-  let newResultTax = Math.floor(resultTax);
-   document.querySelector('#tax').innerText = newResultTax;
+//    // call tax function
+//    let resultTax = Tax(newSubtotal);
+//   let newResultTax = Math.floor(resultTax);
+//    document.querySelector('#tax').innerText = newResultTax;
 
-   //call TotalAmount function for calculate Total amount
+//    //call TotalAmount function for calculate Total amount
 
-   let resultTotal = TotalAmount(newSubtotal,newResultTax);
-   document.querySelector('#total-amount').innerText = resultTotal;
+//    let resultTotal = TotalAmount(newSubtotal,newResultTax);
+//    document.querySelector('#total-amount').innerText = resultTotal;
 
 })
 
@@ -35,34 +45,43 @@ plusIcon.addEventListener('click',(e)=>{
 let minusIcon = document.querySelector('#icon-minus');
 minusIcon.addEventListener('click',(e)=>{
     e.preventDefault();
-    let phoneInputMinus = document.querySelector('#phone-input').value;
-    let phoneInputMinusNumber = parseInt(phoneInputMinus);
-   if( phoneInputMinusNumber > 1){
-    let newPhoneInputMinusNumber = phoneInputMinusNumber - 1;
-    document.querySelector('#phone-input').value = newPhoneInputMinusNumber;
-    // let phoneMinusAmount = document.querySelector('#phone-amount').innerText;
-    // let phoneMinusAmountNumber = parseInt(phoneMinusAmount);
-    let totalMinusPhoneAmount = 1219 * newPhoneInputMinusNumber;
-    document.querySelector('#phone-amount').innerText = totalMinusPhoneAmount;
-    let caseMinusInput = document.querySelector('#case-input').value;
-    let caseMinusInputNumber = parseInt(caseMinusInput);
-    let newMinusSubtotal = updateSubtotal(newPhoneInputMinusNumber,caseMinusInputNumber);
-   document.querySelector('#subtotal').innerText =  newMinusSubtotal;
-   // call tax function
-   let resultTax = Tax(newMinusSubtotal);
-   let newResultTax = Math.floor(resultTax);
-   document.querySelector('#tax').innerText = newResultTax;
+    // let phoneInputMinus = document.querySelector('#phone-input').value;
+    // let phoneInputMinusNumber = parseInt(phoneInputMinus);
+//    if( phoneInputMinusNumber > 1){
+//     // let newPhoneInputMinusNumber = phoneInputMinusNumber - 1;
+//     // document.querySelector('#phone-input').value = newPhoneInputMinusNumber;
+//     // // let phoneMinusAmount = document.querySelector('#phone-amount').innerText;
+//     // // let phoneMinusAmountNumber = parseInt(phoneMinusAmount);
+//     // let totalMinusPhoneAmount = 1219 * newPhoneInputMinusNumber;
+//     // document.querySelector('#phone-amount').innerText = totalMinusPhoneAmount;
+//     let caseMinusInput = document.querySelector('#case-input').value;
+//     let caseMinusInputNumber = parseInt(caseMinusInput);
+//     let newMinusSubtotal = updateSubtotal(newPhoneInputMinusNumber,caseMinusInputNumber);
+//    document.querySelector('#subtotal').innerText =  newMinusSubtotal;
+//    // call tax function
+//    let resultTax = Tax(newMinusSubtotal);
+//    let newResultTax = Math.floor(resultTax);
+//    document.querySelector('#tax').innerText = newResultTax;
 
-   // call Total Amount function for calculate total amount
+//    // call Total Amount function for calculate total amount
    
-   let resultTotal = TotalAmount(newMinusSubtotal,newResultTax);
-   document.querySelector('#total-amount').innerText = resultTotal;
+//    let resultTotal = TotalAmount(newMinusSubtotal,newResultTax);
+//    document.querySelector('#total-amount').innerText = resultTotal;
 
 
-   }
-   else{
-       alert('The number of mobile will not negative');
-   }
+//    }
+//    else{
+//        alert('The number of mobile will not negative');
+//    }
+
+// update code and reduce extra code using function
+productChange('#phone',false);
+let phoneInput = document.querySelector('#phone-input').value;
+let caseInput = document.querySelector('#case-input').value;
+updateSubtotal(phoneInput,caseInput);
+
+
+
    
 })
 
@@ -70,30 +89,41 @@ minusIcon.addEventListener('click',(e)=>{
 let casePlusIcon = document.querySelector('#case-plus');
 casePlusIcon.addEventListener('click',(e)=>{
     e.preventDefault();
-   let caseInput = document.querySelector('#case-input').value;
-   let caseInputNumber = parseInt(caseInput);
-   let newCaseInputNumber = caseInputNumber + 1;
-   document.querySelector('#case-input').value = newCaseInputNumber;
-//    let phoneAmount = document.querySelector('#phone-amount').innerText;
-//    let phoneAmountNumber = parseInt(phoneAmount);
-   let totalCaseAmount = 59 * newCaseInputNumber;
-   document.querySelector('#case-amount').innerText = totalCaseAmount;
+    
+// update code and reduce extra code using more function
+    productChange('#case',true);
+ let phoneInput = document.querySelector('#phone-input').value;
+let caseInput = document.querySelector('#case-input').value;
+updateSubtotal(phoneInput,caseInput);
 
-   // subtotal updateSubtotal
-   let phoneInputPlus = document.querySelector('#phone-input').value;
-   let phoneInputPlusNumber = parseInt(phoneInputPlus);
-   let newPlusSubtotal = updateSubtotal(phoneInputPlusNumber,newCaseInputNumber);
-   document.querySelector('#subtotal').innerText =  newPlusSubtotal;
 
-   //call Tax function to
-   let resultTax = Tax(newPlusSubtotal);
-   let newResultTax = Math.floor(resultTax);
-   document.querySelector('#tax').innerText = newResultTax;
 
-    // call Total Amount function for calculate total amount
+
+
+//    let caseInput = document.querySelector('#case-input').value;
+//    let caseInputNumber = parseInt(caseInput);
+//    let newCaseInputNumber = caseInputNumber + 1;
+//    document.querySelector('#case-input').value = newCaseInputNumber;
+// //    let phoneAmount = document.querySelector('#phone-amount').innerText;
+// //    let phoneAmountNumber = parseInt(phoneAmount);
+//    let totalCaseAmount = 59 * newCaseInputNumber;
+//    document.querySelector('#case-amount').innerText = totalCaseAmount;
+
+//    // subtotal updateSubtotal
+//    let phoneInputPlus = document.querySelector('#phone-input').value;
+//    let phoneInputPlusNumber = parseInt(phoneInputPlus);
+//    let newPlusSubtotal = updateSubtotal(phoneInputPlusNumber,newCaseInputNumber);
+//    document.querySelector('#subtotal').innerText =  newPlusSubtotal;
+
+//    //call Tax function to
+//    let resultTax = Tax(newPlusSubtotal);
+//    let newResultTax = Math.floor(resultTax);
+//    document.querySelector('#tax').innerText = newResultTax;
+
+//     // call Total Amount function for calculate total amount
    
-    let resultTotal = TotalAmount(newPlusSubtotal,newResultTax);
-    document.querySelector('#total-amount').innerText = resultTotal;
+//     let resultTotal = TotalAmount(newPlusSubtotal,newResultTax);
+//     document.querySelector('#total-amount').innerText = resultTotal;
 
 })
 
@@ -102,47 +132,64 @@ casePlusIcon.addEventListener('click',(e)=>{
 let caseMinusIcon = document.querySelector('#case-minus');
 caseMinusIcon.addEventListener('click',(e)=>{
     e.preventDefault();
-    let caseInputMinus = document.querySelector('#case-input').value;
-    let caseInputMinusNumber = parseInt(caseInputMinus);
-   if( caseInputMinusNumber > 1){
-    let newCaseInputMinusNumber = caseInputMinusNumber - 1;
-    document.querySelector('#case-input').value = newCaseInputMinusNumber;
-    // let phoneMinusAmount = document.querySelector('#phone-amount').innerText;
-    // let phoneMinusAmountNumber = parseInt(phoneMinusAmount);
-    let totalMinusCaseAmount = 59 * newCaseInputMinusNumber;
-    document.querySelector('#case-amount').innerText = totalMinusCaseAmount;
+//     let caseInputMinus = document.querySelector('#case-input').value;
+//     let caseInputMinusNumber = parseInt(caseInputMinus);
+//    if( caseInputMinusNumber > 1){
+//     let newCaseInputMinusNumber = caseInputMinusNumber - 1;
+//     document.querySelector('#case-input').value = newCaseInputMinusNumber;
+//     // let phoneMinusAmount = document.querySelector('#phone-amount').innerText;
+//     // let phoneMinusAmountNumber = parseInt(phoneMinusAmount);
+//     let totalMinusCaseAmount = 59 * newCaseInputMinusNumber;
+//     document.querySelector('#case-amount').innerText = totalMinusCaseAmount;
 
-    // subtotal updateSubtotal
-    let phoneInputMinus = document.querySelector('#phone-input').value;
-   let phoneInputMinusNumber = parseInt(phoneInputMinus);
-   let newMinusSubtotal = updateSubtotal(phoneInputMinusNumber,newCaseInputMinusNumber);
-   document.querySelector('#subtotal').innerText =  newMinusSubtotal;
+//     // subtotal updateSubtotal
+//     let phoneInputMinus = document.querySelector('#phone-input').value;
+//    let phoneInputMinusNumber = parseInt(phoneInputMinus);
+//    let newMinusSubtotal = updateSubtotal(phoneInputMinusNumber,newCaseInputMinusNumber);
+//    document.querySelector('#subtotal').innerText =  newMinusSubtotal;
 
 
-   // call tax function
-   let resultTax = Tax(newMinusSubtotal);
-   let newResultTax = Math.floor(resultTax);
-   document.querySelector('#tax').innerText = newResultTax;
+//    // call tax function
+//    let resultTax = Tax(newMinusSubtotal);
+//    let newResultTax = Math.floor(resultTax);
+//    document.querySelector('#tax').innerText = newResultTax;
 
-     // call Total Amount function for calculate total amount
+//      // call Total Amount function for calculate total amount
    
-     let resultTotal = TotalAmount(newMinusSubtotal,newResultTax);
-     document.querySelector('#total-amount').innerText = resultTotal;
+//      let resultTotal = TotalAmount(newMinusSubtotal,newResultTax);
+//      document.querySelector('#total-amount').innerText = resultTotal;
    
-   }
-   else{
-       alert('The number of case will not negative');
-   }
+//    }
+//    else{
+//        alert('The number of case will not negative');
+//    }
+
+
+//update code and reduce extra code using function
+productChange('#case',false);
+let phoneInput = document.querySelector('#phone-input').value;
+let caseInput = document.querySelector('#case-input').value;
+updateSubtotal(phoneInput,caseInput);
+
    
 })
+
+
 // This function use for calculate updateSubtotal
 const updateSubtotal = (phoneInput,caseInput)=>{
-    let updateTotal = (1219 * phoneInput) + (59 * caseInput);
-    return updateTotal;
+    let updateSubtotal = (1219 * phoneInput) + (59 * caseInput);
+    document.querySelector('#subtotal').innerText = updateSubtotal;
+    let resultTax = Tax(updateSubtotal);
+   let updateResultTax =  Math.floor(resultTax);
+   document.querySelector('#tax').innerText = updateResultTax;
+   let totalAmount=TotalAmount(updateSubtotal,updateResultTax);
+   document.querySelector('#total-amount').innerText = totalAmount;
+   
 }
 
-// This function use for update calculate Tax
 
+
+// This function use for update calculate Tax
 const Tax = (subtotal)=>{
     let updateTax = subtotal * 0.1;
     return updateTax;
@@ -235,6 +282,44 @@ caseCross.addEventListener('click',(e)=>{
    
 })
 
+//This is the main function of the project.This function use for increase and decrease input and update value
+function productChange(product,isIncreasing){
+    let productInput = document.querySelector(product + '-input').value;
+    let productConvertNumber = parseInt(productInput);
+    let newProductConvertNumber = 1;
+    let totalProductAmount = 0;
+   if(isIncreasing == true){
+     newProductConvertNumber = productConvertNumber + 1;
+   
+    document.querySelector(product+'-input').value = newProductConvertNumber;
+    if(product == '#phone'){
+        totalProductAmount = 1219 * newProductConvertNumber;
+       
+    }
+    else{
+         totalProductAmount = 59 * newProductConvertNumber;
+    }
+    document.querySelector(product + '-amount').innerText =totalProductAmount;
+
+}
+else{
+     newProductConvertNumber = productConvertNumber - 1;
+    
+    document.querySelector(product+'-input').value = newProductConvertNumber;
+    if(product == '#phone'){
+        totalProductAmount = 1219 * newProductConvertNumber;
+       
+    }
+    else{
+         totalProductAmount = 59 * newProductConvertNumber;
+    }
+    document.querySelector(product + '-amount').innerText =totalProductAmount;
+}
+
+
+}
+
+    
 
 
 
